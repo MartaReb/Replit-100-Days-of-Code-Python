f6c6e48e77b60d4f1cf7779092ca46e270ad142e
MartaReb <49365894+MartaReb@users.noreply.github.com>
2025-02-28 18:05:16 +0100

    Create project_8.py

---
+++ Day 8 - Affirmations/project_8.py
@@ -0,0 +1,22 @@
+print("Hello. Welcome to your daily affirmation generator.")
+name = input("What is your name? ")
+
+if name == "Marta" or name == "marta":
+  day = input("What is the day of the week? ")
+  if day == "Monday" or day == "monday":
+    print(day + "'s affirmation: I am in the right place at the right time, doing the right thing.")
+  if day == "Tuesday" or day == "tuesday":
+    print(day + "'s affirmation: Conscious breathing is my anchor.")
+  if day == "Wednesday" or day == "wednesday":
+    print(day + "'s affirmation: You are loved just for being who you are, just for existing, " + name)
+  if day == "Thursday" or day == "thursday":
+    print(day + "'s affirmation: The chance to love and be loved exists no matter where you are, " + name)
+  if day == "Friday" or day == "friday":
+    print(day + "'s affirmation: Am I good enough? Yes I am.")
+  if day == "Saturday" or day == "saturday":
+    print(day + "'s affirmation: The perfect moment is this one, " + name)
+  if day == "Sunday" or day == "sunday":
+    print(day + "'s affirmation: I am deliberate and afraid of nothing.")
+else:
+ print("I do not know you, but I hope you are having a great day!")
+  
